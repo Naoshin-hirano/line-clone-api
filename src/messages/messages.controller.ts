4e3b79ee9ca81759c6b1a0c7016e453543b0b4db
Hirano <hnaoya1120@gmail.com>
2022-08-08 00:52:07 +0900

    DBなしのCRUD機能

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Message } from './message.model';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
@@ -6,7 +7,42 @@ export class MessagesController {
     constructor(private readonly messagesService: MessagesService) { }
 
     @Get()
-    findAll() {
+    findAll(): Message[] {
         return this.messagesService.findAll();
+    }
+
+    @Get(':id')
+    findById(@Param('id') id: string): Message {
+        return this.messagesService.findById(id);
+    }
+
+    @Post()
+    create(
+        @Body('id') id: string,
+        @Body('postText') postText: string,
+        @Body('username') username: string,
+    ): Message {
+        const message: Message = {
+            id,
+            postText,
+            username,
+        };
+
+        return this.messagesService.create(message);
+    };
+
+    @Patch(':id')
+    update(
+        @Param('id') id: string,
+        @Body('postText') postText: string,
+    ): Message {
+        return this.messagesService.update(id, postText);
+    }
+
+    @Delete(':id')
+    delete(
+        @Param('id') id: string,
+    ): string {
+        return this.messagesService.delete(id);
     }
 }
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { Message } from './message.model';
 
 @Injectable()
 export class MessagesService {
-    findAll() {
-        return 'This is messages service'
+    private messages: Message[] = [];
+
+    findAll(): Message[] {
+        return this.messages;
+    }
+
+    findById(id: string): Message {
+        const found = this.messages.find((item) => item.id === id);
+        return found;
+    }
+
+    create(message: Message): Message {
+        this.messages.push(message);
+        return message;
+    }
+
+    update(id: string, postText: string): Message {
+        const message = this.findById(id);
+        message.postText = postText;
+        return message;
+    }
+
+    delete(id: string): string {
+        this.messages = this.messages.filter((message) => message.id !== id);
+        return '削除完了しました';
     }
 }
